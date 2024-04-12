b0c1b7dea7c0fb5faa9c5236de4be811ed226532
shangcom <dndbs91@gmail.com>
2024-04-12 13:58:16 +0900

    깃허브

---
+++ src/main/java/com/green/bookstore/goods/controller/GoodsControllerImpl.java
@@ -0,0 +1,35 @@
+package com.green.bookstore.goods.controller;
+
+import com.green.bookstore.common.base.BaseController;
+import com.green.bookstore.goods.service.GoodsService;
+import com.green.bookstore.goods.controller.GoodsController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller("goodsController")
+@RequestMapping(value="/goods")
+public class GoodsControllerImpl extends BaseController implements GoodsController {
+
+    @Autowired
+    private GoodsService goodService;
+
+    @Override
+    public ModelAndView goodsDetail(String goods_id, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+
+    @Override
+    public String keywordSearch(String keyword, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ModelAndView searchGoods(String searchWord, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+}
---
+++ src/main/java/com/green/bookstore/goods/dao/GoodsDAO.java
@@ -0,0 +1,16 @@
+package com.green.bookstore.goods.dao;
+
+import com.green.bookstore.goods.vo.GoodsVO;
+import com.green.bookstore.goods.vo.ImageFileVO;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
+public interface GoodsDAO {
+    public List<GoodsVO> selectGoodsList(String goodsStatus) throws DataAccessException;
+    public List<String> selectKeywordsSearch(String keyword) throws DataAccessException;
+    public GoodsVO selectGoodsDetail(String goods_id) throws DataAccessException;
+    public List<ImageFileVO> selectGoodsDetailImage(String goods_id) throws DataAccessException;
+    public List<GoodsVO> selectGoodsBySearchWord(String searchWord) throws DataAccessException;
+
+}
